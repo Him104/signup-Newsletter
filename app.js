3adac0f8a3a0d67f61233224730011cdb02ece4d
Himanshu <52887142+Him104@users.noreply.github.com>
2023-07-10 21:03:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser= require("body-parser");
 const request = require("request");
 
+const https = require("https");
+
 
 
 const app = express();
@@ -22,7 +24,53 @@ app.post("/",function(req,res){
 
     console.log(fname,lname,email);
 
+    const data = {
+        members:[
+            {
+        email_address : email,
+        status: "subscribed",
+        merge_fields:{
+            FNAME: fname,
+            LNAME:lname
+     } }]
+    }
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us21.api.mailchimp.com/3.0/lists/19c0be389d";
+
+    const options = {
+        method:"post",
+        headers:{
+            Authorization : "auth bbf43c4a55506efe8fb15b98fd573041-us21"
+        }
+    }
+
+    const request = https.request(url,options,function(response){
+        if (response.statusCode===200) {
+            res.sendFile(__dirname + "/success.html");
+            
+        }
+        else{
+            res.sendFile(__dirname + "./failure.html")
+        }
+    
+    response.on("data",function(data){
+        console.log(JSON.parse(data));
+    })
+    
 })
+
+request.write(jsonData);
+request.end();
+
+
+});
+
+app.post("/failure",function (req,res) {
+    res.redirect("/");
+    
+});
 
 app.listen(9999,function(){
     console.log("server is running on port 9999");
